af9bdc3ba4f0f01a5399d74b909f279b560e1623
shepherdng <shepherdng@grwth.hk>
2019-04-20 13:08:37 +0800

    Unidirectional Data Flow

--- src/App.js
+++ src/App.js
@@ -26,9 +26,17 @@ class App extends Component {
 
     this.state={
       list,
-    }
+    };
+
+    this.onDismiss = this.onDismiss.bind(this);
   }
   
+  onDismiss(id){
+    this.setState({
+      list: this.state.list.filter(item=>
+        !(item.objectID===id)  
+  )})}
+
   render() {
     return (
       <div className="App">
@@ -40,6 +48,14 @@ class App extends Component {
             <span>{item.author}</span> 
             <span>{item.num_comments}</span> 
             <span>{item.points}</span>
+            <span>
+              <button
+                onClick={()=>this.onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
+            </span>
           </div>
         )}
       </div>
