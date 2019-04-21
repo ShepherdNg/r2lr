a0bf248b80d6b2581f01e3a054d5ebec9905ecaa
shepherdng <shepherdng@grwth.hk>
2019-04-21 17:46:50 +0800

    Fetching Data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const DEFAULT_QUERY = 'redux';
+
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
 
 const isSearched = searchTerm => item =>
   item.title.toLowerCase().includes(searchTerm.toLowerCase()); 
@@ -28,11 +15,24 @@ class App extends Component {
     super(props);
 
     this.state={
-      list,
-      searchTerm: '',
+      result: null,
+      searchTerm: DEFAULT_QUERY,
     };
   }
   
+  setSearchTopStories = (result) => {
+    this.setState({result});
+  }
+
+  componentDidMount() {
+    const {searchTerm} = this.state;
+
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
+  }
+
   onSearchChange = (event)=>{
     this.setState({searchTerm: event.target.value})
   }
@@ -46,7 +46,8 @@ class App extends Component {
   }
 
   render() {
-    const {list, searchTerm} = this.state;
+    const {result, searchTerm} = this.state;
+    if (!result) {return null;}
     return (
       <div className="page">
         <div className="interactions">
@@ -58,7 +59,7 @@ class App extends Component {
           </Search>
         </div>
         <Table
-          list={list}
+          list={result.hits}
           pattern={searchTerm}
           onDismiss={this.onDismiss}
         />
