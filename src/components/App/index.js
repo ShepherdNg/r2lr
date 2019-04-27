1dba5cd55e51c17a0b3c2018e0c70cad0dbcaa30
shepherdng <shepherdngky@gmail.com>
2019-04-28 05:42:23 +0800

    Loading...

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -11,6 +11,7 @@ import {
   PARAM_HPP
 } from '../../constants';
 import {Button} from '../Button';
+import {Loading} from '../Loading';
 import {Search} from '../Search';
 import {Table} from '../Table';
 
@@ -23,6 +24,7 @@ class App extends Component {
       searchKey: '',
       searchTerm: DEFAULT_QUERY,
       error: null,
+      isLoading: false,
     };
   }
   
@@ -46,11 +48,14 @@ class App extends Component {
       results: {
         ...results,
         [searchKey]:{hits: updatedHits, page}
-      }
+      },
+      isLoading:false
     });
   }
 
 	fetchSearchTopStories = (searchTerm, page=0) => {
+    this.setState({isLoading:true});
+
     axios(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(result => this.setSearchTopStories(result.data))
       .catch(error => this.setState({error}));
@@ -95,7 +100,8 @@ class App extends Component {
       searchTerm,
       results,
       searchKey,
-      error
+      error,
+      isLoading
     } = this.state;
 
     const page = (
@@ -129,13 +135,16 @@ class App extends Component {
           />
         }
         <div className="interactions">
-          <Button onClick={()=>this.fetchSearchTopStories(searchKey, page+1)}>
-            More
-          </Button>
+        { isLoading
+          ? <Loading />
+          : <Button onClick={()=>this.fetchSearchTopStories(searchKey, page+1)}>
+              More
+            </Button>
+        }
         </div>
       </div>
     );
   }
 }
-  
+
 export default App;
---
+++ src/components/Loading/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+export const Loading = () =>
+  <div><i className="fas fa-spinner"></i></div>
