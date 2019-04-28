bfa5454d44e612df8ff307328be8d3098bfbab31
shepherdng <shepherdngky@gmail.com>
2019-04-29 01:30:27 +0800

    Higher-Order Components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -135,16 +135,23 @@ class App extends Component {
           />
         }
         <div className="interactions">
-        { isLoading
-          ? <Loading />
-          : <Button onClick={()=>this.fetchSearchTopStories(searchKey, page+1)}>
-              More
-            </Button>
-        }
+          <ButtonWithLoading
+            isLoading={isLoading}
+            onClick={()=>this.fetchSearchTopStories(searchKey, page+1)}
+          >
+            More
+          </ButtonWithLoading>
         </div>
       </div>
     );
   }
 }
+
+const withLoading = (Component) => ({isLoading,...rest}) =>
+  isLoading
+    ? <Loading />
+    : <Component {...rest} />
+
+const ButtonWithLoading = withLoading(Button);
 
 export default App;
